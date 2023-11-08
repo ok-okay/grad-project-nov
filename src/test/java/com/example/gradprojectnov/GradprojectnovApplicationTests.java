eedc5c8f5b35a2608d7a75049368142845c68e84
lalit <jonsnow1762@gmail.com>
2023-11-08 07:33:47 +0530

    'initial-template'

---
+++ src/test/java/com/example/gradprojectnov/GradprojectnovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradprojectnov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradprojectnovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
